6b7b56a23f3de5c7b3483883218362e2f473d28c
hiimparkjiho <jiho2689@gmail.com>
2023-10-31 00:45:04 +0900

    inheritance

---
+++ chap08_inheritance_lecture_source/src/main/java/com/ohgiraffer/vendingmachine/Application.java
@@ -0,0 +1,4 @@
+package main.java.com.ohgiraffer.vendingmachine;
+
+public class Application {
+}
---
+++ chap08_inheritance_lecture_source/src/main/java/com/ohgiraffer/vendingmachine/vendingmachine.java
@@ -0,0 +1,11 @@
+package main.java.com.ohgiraffer.vendingmachine;
+
+import java.util.Scanner;
+
+public class vendingmachine {
+
+    private String drink;
+
+    private int price;
+
+}
