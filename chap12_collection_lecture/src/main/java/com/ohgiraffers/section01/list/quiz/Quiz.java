91462f96032389e4cae3e89c5de01dd725d24576
hiimparkjiho <jiho2689@gmail.com>
2023-11-03 01:07:52 +0900

    list

--- chap12_collection_lecture/src/main/java/com/ohgiraffers/section01/list/quiz/Quiz.java
+++ chap12_collection_lecture/src/main/java/com/ohgiraffers/section01/list/quiz/Quiz.java
@@ -8,17 +8,32 @@ public class Quiz {
         //문제: 사용자로부터 숫자를 입력받아 이를 ArrayList에 저장하고, 입력이 끝나면 ArrayList의 모든 숫자를 출력하는 Java 프로그램을 작성하세요.
 
         Scanner scan = new Scanner(System.in);
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        int input;
+//        ArrayList<Integer> arrayList = new ArrayList<>();
+//        int input;
+//        for(;;){
+//            System.out.println("숫자를 입력해주세요 :   (입력 종료는 -1)");
+//            input = scan.nextInt();
+//            if(input == -1){
+//                System.out.println("입력 종료");
+//                break;
+//            }
+//            arrayList.add(input);
+//        }
+//        System.out.println(arrayList);
+
+        //문제: 사용자로부터 문자열을 입력받아 이를 ArrayList에 저장한 후, 입력이 끝나면 ArrayList의 문자열을 알파벳 순으로 정렬하고 출력하는 Java 프로그램을 작성하세요.
+        ArrayList<String> stringList = new ArrayList<>();
+        String result;
         for(;;){
-            System.out.println("숫자를 입력해주세요 :   (입력 종료는 -1)");
-            input = scan.nextInt();
-            if(input == -1){
-                System.out.println("입력 종료");
+            System.out.println("알파벳을 입력해주세요: ");
+            result = scan.next();
+
+            if(result.equals("end")){
+                System.out.println("프로그램을 종료합니다.");
                 break;
             }
-            arrayList.add(input);
+            stringList.add(result);
         }
-        System.out.println(arrayList);
+        System.out.println(stringList);
     }
 }
