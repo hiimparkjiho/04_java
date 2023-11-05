ae5d6df4e50369f7d976a717f2b151dd3015f244
hiimparkjiho <jiho2689@gmail.com>
2023-11-05 23:04:33 +0900

    quiz

--- chap12_collection_lecture/src/main/java/com/ohgiraffers/understand/Application02.java
+++ chap12_collection_lecture/src/main/java/com/ohgiraffers/understand/Application02.java
@@ -14,21 +14,42 @@ public class Application02 {
         Map<String, Set<Integer>> lotto = new HashMap<>();
         Random random = new Random();
         Scanner scan = new Scanner(System.in);
-
+        int totalLottoGenerated = 0;    // 구매한 로또 개수
         while (true){
-            Set<Integer> lottoNum = new HashSet<>();
             System.out.println("구매할 복권을 개수를 입력해주세요 (최대 10개까지 구매할 수 있습니다.) : ");
             int num = scan.nextInt();
-
-           if(num <= 10){
-               System.out.println(lottoNum);
-               break;
+           if(num <= 0 || num > 10){
+               System.out.println("다시 시도해주세요"); // 0개 이하로 사거나 10개 넘어가서 구매시 취소
+               return;
+           }else{
+               for(int i = 1; i <= num; i++){
+                   Set<Integer> lottoNum = new HashSet<>();
+                   while(lottoNum.size() < 6){
+                       int luckyNum = random.nextInt(45) + 1;// 복권 행운번호 랜덤
+                       lottoNum.add(luckyNum);// 세트에 값 대입
+                   }
+                   System.out.println(i + "번째 행운번호: " + lottoNum);
+                   totalLottoGenerated++; //증감 연산자로 구매할 때마다 구매한 로또 개수 증가
+                   if (totalLottoGenerated >= 10) {
+                       System.out.println("복권 번호 10개를 모두 생성했습니다.");
+                       Set<Integer> winningNumber = new HashSet<>();
+                       while (winningNumber.size() < 6){
+                           int winningNum = random.nextInt(45) + 1;
+                           winningNumber.add(winningNum);
+                       }
+                       if(lottoNum.equals(winningNumber)){
+                           System.out.println("당첨번호: " + winningNumber);
+                           System.out.println("당첨되었습니다.");
+                           return;
+                       }else{
+                           System.out.println("당첨번호: " + winningNumber);
+                           System.out.println("당첨자가 없습니다.");
+                           return; // 프로그램 종료
+                       }
+                   }
+               }
            }
 
-            while(lottoNum.size() < 6){
-                int luckyNum = random.nextInt(45) + 1;
-                lottoNum.add(luckyNum);
-            }
         }
 
 
